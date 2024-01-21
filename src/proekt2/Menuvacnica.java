190f55d2f31464bf829c4d104d513878940a1432
Jasin Ibishi <jasinibishi@gmail.com>
2024-01-21 14:43:14 +0100

    first commit

---
+++ src/proekt2/Menuvacnica.java
@@ -0,0 +1,15 @@
+package proekt2;
+
+public class Menuvacnica {
+	public static void main(String[] args) {
+		double evra=500;
+		double denari_vrednost=61.7;
+		double rezultat=evra*denari_vrednost;
+		
+		System.out.println("Zadadena vrednost za evra : ");
+		System.out.println(evra);
+		System.out.print("Pretvoreni vo denari se : ");
+		System.out.print(rezultat);
+	}
+}
+
